5b299fc6855f735a419ec54f2e127cabd8481c67
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-03-08 08:46:39 -0800

    route-redux

---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { getSmurfs } from '../actions';
+
+class SmurfList extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Smurf List</h1>
+            </div>
+        )
+    }
+}
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,23 +1,29 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import { ERROR, GETTING_SMURFS, GET_SMURFS } from '../actions';
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
+const initialState = {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
+   gettingSmurfs: false,
+  //  addingSmurf: false,
+  //  updatingSmurf: false,
+  //  deletingSmurf: false,
    error: null
- }
-*/
+ };
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
-*/
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GETTING_SMURFS:
+      return{ ...state, gettingSmurfs: true };
+    case GET_SMURFS:
+      return{ ...state, smurfs: action.payload, gettingSmurfs: false };   
+    case ERROR:
+      return {
+        ...state,
+        gettingSmurfs: false,
+        error: action.payload
+      }    
+    default: 
+      return state;   
+  }
+};
+
+export default reducer;
